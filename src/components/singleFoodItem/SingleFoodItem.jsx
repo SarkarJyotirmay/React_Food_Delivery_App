7876350a4d92b9fa5ebf02416953a0197d77e879
SarkarJyotirmay <jsarkar0201@gmail.com>
2025-05-12 13:02:30 +0530

    fixed css for hero, replaced single button for single food item

--- src/components/singleFoodItem/SingleFoodItem.jsx
+++ src/components/singleFoodItem/SingleFoodItem.jsx
@@ -3,21 +3,26 @@ import styles from "./single_food_item.module.css";
 import { assets } from "../../assets/assets";
 import { useDispatch, useSelector } from "react-redux";
 
-import { displayCart, addFirst, increaseQuantity, decreaseQuantity } from "../../store/slices/cartSlice";
+import {
+  displayCart,
+  addFirst,
+  increaseQuantity,
+  decreaseQuantity,
+} from "../../store/slices/cartSlice";
 import { useNavigate } from "react-router-dom";
 
 function SingleFoodItem({ obj }) {
   const [orderQuantity, setOrderQuantity] = useState(0);
 
-  const dispatch = useDispatch()
-  const cart = useSelector((state)=>state.cartReducers)
-  let existing = cart.find((object)=>obj._id === object._id)
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cartReducers);
+  let existing = cart.find((object) => obj._id === object._id);
 
   // useEffect(()=>{console.log(cart);
   // },[cart])
 
-  const navigate = useNavigate()
-  
+  const navigate = useNavigate();
+
   return (
     <div className={styles["single-food-item"]}>
       <div className={styles.image}>
@@ -35,16 +40,19 @@ function SingleFoodItem({ obj }) {
           <img
             src={assets.add_icon_green}
             alt=""
-           onClick={()=>dispatch(increaseQuantity(obj))}
+            onClick={() => dispatch(increaseQuantity(obj))}
           />
         </div>
       ) : (
-        <img
-          src={assets.add_icon_white}
-          alt=""
-          className={styles.add}
-          onClick={() => dispatch(addFirst(obj))}
-        />
+        // <img
+        //   src={assets.add_icon_white}
+        //   alt=""
+        //   className={styles.add}
+        //   onClick={() => dispatch(addFirst(obj))}
+        // />
+        <button className={styles.add} onClick={() => dispatch(addFirst(obj))}>
+          Add to Cart
+        </button>
       )}
 
       <div className={styles.info}>
@@ -52,8 +60,12 @@ function SingleFoodItem({ obj }) {
         <p>{obj.description}</p>
         <div className={styles["cart-actions"]}>
           <h3>$ {obj.price}</h3>
-          <button onClick={()=>navigate("/cart")} className={styles["add-to-cart"]}>Go To Cart</button>
-          
+          <button
+            onClick={() => navigate("/cart")}
+            className={styles["add-to-cart"]}
+          >
+            Go To Cart
+          </button>
         </div>
       </div>
     </div>
